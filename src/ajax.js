96b0c13c117adb6d6787d4a78a33cda8d0f17703
skmahato <33520110+skmahato@users.noreply.github.com>
2019-04-16 12:49:43 +0530

    moved contacts related api calls to a diffrent directory (#2)

--- src/ajax.js
+++ src/ajax.js
@@ -1,37 +1,15 @@
-const url = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ?
-  'http://localhost:3001/contacts' :
-  'https://reactjs-contact-list-api.herokuapp.com/contacts';
+function buildParam(params) {
+  let fD = new FormData();
+  Object.keys(params).map(param => fD.append(param, params[param]));
+  return fD;
+}
 
-export const getContacts = () => fetch(url).then(response => response.json());
-
-export const newContact = (input) => {
-  const options = { method: "POST", body: buildParam(input) };
+export const ajax = (url, options = null) => {
+  if (options && options.body) {
+    options.body = buildParam(options.body);
+  }
 
   return fetch(url, options).then(response => response.json());
 }
 
-export const updateContact = (val, input) => {
-  const options = {
-    method: "PUT",
-    body: buildParam(input),
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  };
-
-  return fetch(`${url}/${val.id}`, options).then(response => response.json());
-};
-
-export const deleteContact = (val) => {
-  const options = { method: "DELETE" };
-
-  return fetch(`${url}/${val.id}`, options).then(response => response.json());
-};
-
-function buildParam(params) {
-  let fD = new FormData();
-  Object.keys(params).map(param => {
-    return fD.append(param, params[param]);
-  });
-  return fD;
-}
+export default ajax;
